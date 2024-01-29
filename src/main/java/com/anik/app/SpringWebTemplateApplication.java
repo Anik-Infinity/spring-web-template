6e0d88f25dad9b5844f55ffd48604618fc7a67ec
Kafi <kafi@itconquest.com>
2024-01-30 01:05:05 +0600

    hibernate validator added to pom.xml

--- src/main/java/com/anik/app/SpringWebTemplateApplication.java
+++ src/main/java/com/anik/app/SpringWebTemplateApplication.java
@@ -1,18 +1,11 @@
 package com.anik.app;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
-
 @SpringBootApplication
-@Slf4j
 public class SpringWebTemplateApplication {
-
 	public static void main(String[] args) {
-		log.info("Welcome {}", LocalDate.now());
 		SpringApplication.run(SpringWebTemplateApplication.class, args);
 	}
-
 }
